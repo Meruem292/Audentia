790b115456a7fb9cc1c12c9c15b9a4a3c7d0566f
MHERWEN WIEL CALUMBA ROMERO <mwcromero@kld.edu.ph>
2025-11-07 07:23:17 +0000

    Error: [ Server ] Error fetching admin transaction history: {}     at cr

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -8,6 +8,7 @@ if (!admin.apps.length) {
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
         privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
       }),
+      databaseURL: process.env.FIREBASE_DATABASE_URL,
     });
   } catch (error) {
     console.error('Firebase admin initialization error', error);
