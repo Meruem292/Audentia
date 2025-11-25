1532fcf10d31590fbeceeb851a1b574c22daa9c8
MHERWEN WIEL CALUMBA ROMERO <mwcromero@kld.edu.ph>
2025-11-25 08:51:10 +0000

    remove the logo and EcoVend text in the navbar since we already have in

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -2,8 +2,6 @@
 'use client';
 import { UserNav } from "@/components/dashboard/user-nav";
 import { SidebarTrigger } from "@/components/ui/sidebar";
-import { Logo } from "./logo";
-import Link from "next/link";
 import { useUser } from "@/lib/firebase";
 
 export function Header({ title }: { title: string }) {
@@ -13,12 +11,7 @@ export function Header({ title }: { title: string }) {
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <div className="flex gap-2 items-center">
             <SidebarTrigger className="md:hidden" />
-            <div className="hidden md:block">
-              <Link href="/">
-                <Logo />
-              </Link>
-            </div>
-            <h1 className="text-xl font-bold tracking-tight hidden sm:block ml-4">{title}</h1>
+            <h1 className="text-xl font-bold tracking-tight">{title}</h1>
         </div>
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-1">
